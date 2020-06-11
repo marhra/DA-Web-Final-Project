930252b2d0b4bbad0af4a3f8800ae9fac29c5503
marhra <64075823+marhra@users.noreply.github.com>
2020-06-11 18:12:01 +0200

    Starter pack uploaded

---
+++ src/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <aside class="menu">
+    <div class="menu__item">Domů</div>
+    <div class="menu__item">Blog</div>
+    <div class="menu__item">Kontakt</div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+};
+</script>
+
+<style lang="css">
+.menu {
+  padding-top: 2rem;
+  flex: 0 0 12rem;
+  width: 12rem;
+  height: 100%;
+  background-color: deepskyblue;
+  color: #555;
+}
+
+.menu__item {
+  line-height: 2rem;
+  padding: 0 2rem;
+}
+</style>
