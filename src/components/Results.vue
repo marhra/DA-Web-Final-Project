b0121676e09e6ca9aa54f572df1606374b884135
Zdeněk Suda <zsuda@vccp.com>
2020-06-21 14:56:27 +0200

    Create page Results

---
+++ src/components/Results.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="results">
+        results
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Results",
+        components: {}
+    };
+</script>
