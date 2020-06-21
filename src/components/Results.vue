bfaceb4b1e3a946adcf968ee5d343666bd3cb273
Zdeněk Suda <zsuda@vccp.com>
2020-06-21 16:03:29 +0200

    Results.vue WIP

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="results">
         results
+    
+    
+        <li v-for="(item, index) in obj" v-bind:key="index">
+            {{ item.breakfast }}
+        </li>
+        
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import createMenu from '../vypocet';
+    
     export default {
         name: "Results",
-        components: {}
+        data: obj,
+        mounted() {}
     };
 </script>
