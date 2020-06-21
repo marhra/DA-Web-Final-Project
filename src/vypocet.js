408468505652f2f3b4aa2965b753b90d08c000eb
Marie Hrabovska <marie.hrabovska@gmail.com>
2020-06-21 14:29:40 +0200

    Vypocet js done

--- src/vypocet.js
+++ src/vypocet.js
@@ -1,8 +1,10 @@
-const data = require("./json/data.json");
+const data = require("./json/data.json")
 
-const recipes = Object.values(data);
+const recipes = Object.values(data).map(e => ({ ...e, caloriesPerPortion: e.calories / e.yield }));
 
-const breakfasts = recipes.filter(recipe => recipe.mealType === "Breakfast");
+const userInput = 1500;
+
+const breakfasts = recipes.filter(recipe => recipe.mealType === "Breakfast").filter(recipe => recipe.caloriesPerPortion > 0.2 * userInput && recipe.caloriesPerPortion < 0.4 * userInput)
 
 const lunches = recipes.filter(recipe => recipe.mealType === "Main dish")
 
@@ -26,8 +28,32 @@ const lunchIndex = Math.floor(Math.random() * (lunches.length - 1))
 const lunch = lunches[lunchIndex]
 //console.log(lunch)
 
-const caloriesWithoutDinner = breakfast.calories / breakfast.yield + lunch.calories / lunch.yield;
+const caloriesWithoutDinner = breakfast.caloriesPerPortion + lunch.caloriesPerPortion;
 
-const missingCalories = 1800 - caloriesWithoutDinner;
-console.log(missingCalories)
+const missingCalories = userInput - caloriesWithoutDinner;
+//console.log(missingCalories)
 
+const lunchesCalories = lunches.map(e => e.calories / e.yield)
+
+
+for (var i = 0, currentClosestNum = 10000, currentClosestIndex = null; i < lunchesCalories.length; i++) {
+
+
+  if (Math.abs(lunchesCalories[i] - missingCalories) < currentClosestNum) {
+    currentClosestNum = Math.abs(lunchesCalories[i] - missingCalories);
+    currentClosestIndex = i
+  }
+}
+
+const dinner = lunches[currentClosestIndex];
+
+// console.log(dinner)
+
+//console.log(currentClosestIndex, currentClosestNum);
+
+//console.log(lunches[currentClosestIndex], lunches[currentClosestIndex].calories / lunches[currentClosestIndex].yield, missingCalories)
+
+
+const total = caloriesWithoutDinner + dinner.caloriesPerPortion;
+
+console.log(total)
