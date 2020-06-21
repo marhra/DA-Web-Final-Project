3390879242018bf70b1bbbab335ac1b4b9a2c1cb
Zdeněk Suda <zsuda@vccp.com>
2020-06-21 16:53:52 +0200

    Results.vue listing meals

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,9 +1,17 @@
 <template>
     <div class="results">
-        <div v-for="(item, index) in items" v-bind:key="index">
-            <h3>{{ item.day }}</h3>
-            <p>{{ item }}</p>
-            
+        <div class="mb-5" v-for="(item, index) in items" v-bind:key="index">
+            <h2>{{ item.day }}</h2>
+            <div>
+                <h4>{{item.breakfast.label}}</h4>
+                <p><small>{{ Math.round(item.breakfast.caloriesPerPortion) }} kCal</small></p>
+    
+                <h4>{{item.lunch.label}}</h4>
+                <p><small>{{ Math.round(item.lunch.caloriesPerPortion) }} kCal</small></p>
+    
+                <h4>{{item.dinner.label}}</h4>
+                <p><small>{{ Math.round(item.dinner.caloriesPerPortion) }} kCal</small></p>
+            </div>
         </div>
         
         <router-view></router-view>
@@ -17,11 +25,12 @@
     let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     
     for (let i = 0; i < 7; i++) {
+        console.log(i, createMenu());
         let item = {
             day: days[i],
-            breakfast: createMenu().breakfast.label,
-            lunch: createMenu().lunch.label,
-            dinner: createMenu().dinner.label
+            breakfast: createMenu().breakfast,
+            lunch: createMenu().lunch,
+            dinner: createMenu().dinner
         };
         
         obj.push(item);
